5661ce3bf195332a899a023768f0f30f24f3dd18
Mikelesnr <mwanza.n.m@gmail.com>
2022-12-17 21:49:03 +0200

    display all minerals implemented

--- frontend/pocket-geologist/src/components/pages/Display.js
+++ frontend/pocket-geologist/src/components/pages/Display.js
@@ -1,19 +1,20 @@
 import Header from '../common/Header';
 import axios from 'axios';
 import React,{useState,useEffect} from 'react';
-import Table from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
 
 function Display() {
     const [minerals,setMinerals]=useState([]);
 
     useEffect(()=>{
         fetchData();
-    },[]);
+        },[]);
 
 
     const fetchData = async ()=>{
         const {data} = await axios.get("http://127.0.0.1:8000/api/displayAll");
         setMinerals(data);
+        
     }
 
     console.log(minerals);
@@ -21,7 +22,37 @@ function Display() {
     return(
         <>
         <Header/>
+        <div className='col-sm-8 offset-sm-2'>
         <h1>Minerals</h1>
+        <Table>
+            <thead className='hide'>
+            <tr>
+                <th>name</th>
+                <th>group</th>
+                <th>description</th>
+                <th>image</th>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    minerals.map((mineral)=>
+                    <tr>
+                            <td className='hide'><h4>{mineral.mineral}</h4></td>
+                            <td className='hide'>{mineral.group}</td>
+                            <td className='hide'><p>{mineral.description}</p></td>
+                            <td>
+                                <img className='min-pic' src={"http://localhost:8000/"+mineral.image_path} alt="Mineral pic"/>
+                                <h4 className='hideBig'>Name: {mineral.mineral}</h4>
+                                <h5 className='hideBig'>Group: {mineral.group}</h5>
+                                <p className='hideBig'><b>Description: </b>{mineral.description}</p>
+                            </td>
+                        </tr>    
+                    )
+                }
+            </tbody>            
+        </Table>
+        </div>
+        
         </>
     )
 
